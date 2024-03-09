f59597ca4b1ea0e26f2f353f66b3f0c89e690241
darsh patel <pdars@MacBook-Pro.local>
2024-03-09 17:51:55 -0600

    full project files

---
+++ ecommerce website-Patel's store/includes/footer.php
@@ -0,0 +1,10 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2020</strong>
+	  <br>
+	  <strong> Brought to You By Darsh Patel and Hewitt Kothari</strong>
+    </div>
+</footer>
